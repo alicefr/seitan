726e4d2538e7ff8293bc4a98599473e266f26d4b
Alice Frosi <afrosi@redhat.com>
2022-12-19 17:15:35 +0100

    Add unit tests

--- tests/unit/test-filter.c
+++ tests/unit/test-filter.c
@@ -8,6 +8,8 @@
 #include "../../filter.h"
 #include "../../disasm.h"
 
+long nr = 42;
+
 static bool filter_eq(struct sock_filter *f1, struct sock_filter *f2,
 		      unsigned int n)
 {
@@ -15,8 +17,9 @@ static bool filter_eq(struct sock_filter *f1, struct sock_filter *f2,
 	for (i = 0; i < n; i++) {
 		if (memcmp((void *)(f1 + i), (void *)(f2 + i),
 			   sizeof(struct sock_filter)) != 0) {
-			printf("position %d not equal\n", i);
+			printf("expected:\n");
 			bpf_disasm(f1[i], i);
+			printf("got:\n");
 			bpf_disasm(f2[i], i);
 			return false;
 		}
@@ -27,14 +30,8 @@ static bool filter_eq(struct sock_filter *f1, struct sock_filter *f2,
 START_TEST(test_single_instr)
 {
 	unsigned int size;
-	long nr = 42;
 	struct bpf_call calls[] = {
-		{
-			.name = "connect",
-			.args = { 0, 0, 0, 0, 0, 0 },
-			.check_arg = { false, false, false, false, false,
-				       false },
-		},
+		{ .name = "test1" },
 	};
 	struct syscall_entry table[] = {
 		{ .count = 1, .nr = nr, .entry = &calls[0] },
@@ -66,7 +63,7 @@ START_TEST(test_single_instr_two_args)
 	long nr = 42;
 	struct bpf_call calls[] = {
 		{
-			.name = "connect",
+			.name = "test1",
 			.args = { 0, 123, 321, 0, 0, 0 },
 			.check_arg = { false, true, true, false, false, false },
 		},
@@ -103,36 +100,32 @@ START_TEST(test_two_instr)
 	struct bpf_call calls[] = {
 		{
 			.name = "connect",
-			.args = { 0, 0, 0, 0, 0, 0 },
-			.check_arg = { false, false, false, false, false,
-				       false },
 		},
 		{
 			.name = "bind",
-			.args = { 0, 0, 0, 0, 0, 0 },
-			.check_arg = { false, false, false, false, false,
-				       false },
 		},
 	};
 	struct syscall_entry table[] = {
 		{ .count = 1, .nr = 42, .entry = &calls[0] },
 		{ .count = 1, .nr = 49, .entry = &calls[1] },
 	};
-	struct sock_filter result[10];
 	struct sock_filter expected[] = {
 		/* l0 */ BPF_STMT(BPF_LD | BPF_W | BPF_ABS,
 		 	 (offsetof(struct seccomp_data, arch))),
 		/* l1 */ BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, SEITAN_AUDIT_ARCH, 0, 6),
 		/* l2 */ BPF_STMT(BPF_LD | BPF_W | BPF_ABS,
 	 	 (offsetof(struct seccomp_data, nr))),
+		/* ------- level0 -------- */
 		/* l3 */ JGE(49, 2, 0),
+		/* ------- level0 -------- */
 		/* l4 */ EQ(42, 0, 3),
-		/* l5 */ JUMPA(0),
+		/* l5 */ JUMPA(2),
 		/* l6 */ EQ(49, 0, 1),
 		/* l7 */ JUMPA(0),
 		/* l8 */ BPF_STMT(BPF_RET | BPF_K, SECCOMP_RET_ALLOW),
 		/* l9 */ BPF_STMT(BPF_RET | BPF_K, SECCOMP_RET_USER_NOTIF),
 	};
+	struct sock_filter result[30];
 
 	size = create_bfp_program(table, result,
 				  sizeof(calls) / sizeof(calls[0]));
@@ -141,6 +134,59 @@ START_TEST(test_two_instr)
 			    sizeof(expected) / sizeof(expected[0])));
 }
 END_TEST
+
+START_TEST(test_multiple_instr_no_args)
+{
+	unsigned int size;
+	struct bpf_call calls[] = { { .name = "test1"}, { .name = "test2"},
+		{ .name = "test3"}, { .name = "test4"}, { .name = "test5"},
+	};
+	struct syscall_entry table[] = {
+		{ .count = 1, .nr = 42, .entry = &calls[0] },
+		{ .count = 1, .nr = 43, .entry = &calls[1] },
+		{ .count = 1, .nr = 44, .entry = &calls[2] },
+		{ .count = 1, .nr = 45, .entry = &calls[3] },
+		{ .count = 1, .nr = 46, .entry = &calls[4] },
+	};
+	struct sock_filter expected[] = {
+		/* l0 */ BPF_STMT(BPF_LD | BPF_W | BPF_ABS,
+		 	 (offsetof(struct seccomp_data, arch))),
+		/* l1 */ BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, SEITAN_AUDIT_ARCH, 0, 17),
+		/* l2 */ BPF_STMT(BPF_LD | BPF_W | BPF_ABS,
+	 	 (offsetof(struct seccomp_data, nr))),
+		/* ------- level0 -------- */
+		/* l3 */ JGE(46, 1, 0),
+		/* ------- level1 -------- */
+		/* l4 */ JGE(45, 2, 1),
+		/* l5 */ JGE(46, 3, 3),
+		/* ------- level2 -------- */
+		/* l6 */ JGE(43, 5, 2),
+		/* l7 */ JGE(45, 9, 6),
+		/* l8 */ JGE(46, 8, 8),
+		/* -------- leaves ------- */
+		/* l9 */  EQ(42, 10, 9),
+		/* l10 */ JUMPA(8),
+		/* l11 */ EQ(43, 8, 7),
+		/* l12 */ JUMPA(6),
+		/* l13 */ EQ(44, 6, 5),
+		/* l14 */ JUMPA(4),
+		/* l15 */ EQ(45, 4, 3),
+		/* l16 */ JUMPA(2),
+		/* l17 */ EQ(46, 2, 1),
+		/* l18 */ JUMPA(0),
+		/* l19 */ BPF_STMT(BPF_RET | BPF_K, SECCOMP_RET_ALLOW),
+		/* l20 */ BPF_STMT(BPF_RET | BPF_K, SECCOMP_RET_USER_NOTIF),
+	};
+	struct sock_filter result[sizeof(expected) / sizeof(expected[0]) + 10];
+
+	size = create_bfp_program(table, result,
+				  sizeof(calls) / sizeof(calls[0]));
+	ck_assert_uint_eq(size, sizeof(expected) / sizeof(expected[0]));
+	ck_assert(filter_eq(expected, result,
+			    sizeof(expected) / sizeof(expected[0])));
+}
+END_TEST
+
 
 Suite *bpf_suite(void)
 {
@@ -153,6 +199,7 @@ Suite *bpf_suite(void)
 	tcase_add_test(tc_core, test_single_instr);
 	tcase_add_test(tc_core, test_single_instr_two_args);
 	tcase_add_test(tc_core,test_two_instr);
+	tcase_add_test(tc_core,test_multiple_instr_no_args);
 
 	suite_add_tcase(s, tc_core);
 
