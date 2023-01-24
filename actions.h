ee1108e29570db57aaeeab6ab8e03e3b918355c9
Alice Frosi <afrosi@redhat.com>
2023-01-24 17:05:11 +0100

    seitan: action for the call
    Perform the action action with the context. The action call executes a
    syscall in the given namespaces or in caller context if non is
    specified.
    
    Signed-off-by: Alice Frosi <afrosi@redhat.com>

---
+++ actions.h
@@ -0,0 +1,20 @@
+#ifndef ACIONS_H
+#define ACTIONS_H
+
+#include <errno.h>
+
+#define STACK_SIZE (1024 * 1024 / 8)
+#define NS_NUM (sizeof(enum ns_type))
+
+struct arg_clone {
+        const struct act_call *args;
+        pid_t pid;
+        long ret;
+        int err;
+};
+
+int do_call(struct arg_clone *c);
+int do_actions(struct action actions[], unsigned int n_actions, int tpid,
+                int notifyfd, uint64_t id);
+#endif /* ACTIONS_H */
+
