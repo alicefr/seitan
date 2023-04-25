a2c20b2caf4e9cedb32b0930ffd7ca2e9ec72086
Alice Frosi <afrosi@redhat.com>
2023-04-25 13:46:29 +0200

    filter: add missing license header

--- cooker/filter.c
+++ cooker/filter.c
@@ -1,3 +1,8 @@
+/* SPDX-License-Identifier: GPL-3.0-or-later
+ * Copyright 2023 Red Hat GmbH
+ * Author: Alice Frosi <afrosi@redhat.com>
+ */
+
 #define _GNU_SOURCE
 #include <fcntl.h>
 #include <stdbool.h>
